96e39cd742a4706e065219be2e6bdb71cb96819a
Gene Kelley <phpcodehead@gmail.com>
2025-05-24 17:36:02 -0500

    Add ShopProduct and AddressManager classes. Add ext-simplexml to composer dependencies.

--- app/ObjectBasics/ShopProduct.php
+++ app/ObjectBasics/ShopProduct.php
@@ -3,5 +3,16 @@
 namespace App\ObjectBasics;
 
 class ShopProduct {
-    // class body
+    public function __construct(
+        public string $title = "default product",
+        public string $producerFirstName = "first name",
+        public string $producerMainName = "main name",
+        public float $price = 0
+    ) {}
+
+    public function getProducer(): string
+    {
+        return $this->producerFirstName . " "
+            . $this->producerMainName;
+    }
 }
--- app/object-basics.php
+++ app/object-basics.php
@@ -3,9 +3,17 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\ObjectBasics\ShopProduct;
+use App\ObjectBasics\AddressManager;
 
-$product1 = new ShopProduct();
-$product2 = new ShopProduct();
+$product = new ShopProduct(
+    title: "My Antonia",
+    producerFirstName: "Willa",
+    producerMainName: "Cather",
+    price: 5.99
+);
 
-var_dump($product1);
-var_dump($product2);
+print "author: {$product->getProducer()}" . PHP_EOL;
+
+$settings = simplexml_load_file(__DIR__ . '/settings.xml');
+$manager = new AddressManager();
+$manager->outputAddresses((string) $settings->resolvedomains);
