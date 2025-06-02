046b1ffed3c07a774fd0deeaad9918e31aadfbf8
Gene Kelley <phpcodehead@gmail.com>
2025-06-01 19:48:27 -0500

    Add ShopProduct classes.

--- app/Http/Controllers/AdvancedFeaturesController.php
+++ app/Http/Controllers/AdvancedFeaturesController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AdvancedFeatures\ShopProduct;
 use App\Models\AdvancedFeatures\StaticExample;
+use PDOException;
 
 class AdvancedFeaturesController extends Controller
 {
     public function index()
     {
         $greeting = StaticExample::sayHello();
-        dd($greeting);
+
+        // Create and return a new PDO database connection
+        try {
+            $db_conn = require_once(database_path('connection.php'));
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit;
+        }
+
+        $book = ShopProduct::getInstance(1, $db_conn);
+
+        dd($book);
     }
 }
