8bd062c78c8657d19598e4623687c72783a94121
Gene Kelley <phpcodehead@gmail.com>
2025-09-05 15:01:47 -0500

    Add Tasks route, controller, model, and database migration.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,10 @@
     <div class="mb-4 text-[0.5em]/loose">
         Code examples from PHP 8: Objects, Patterns and Practice, 7th Edition, Volume 1
     </div>
+    <div class="mb-4 text-[0.5em]/loose">
+        <a href="<?php echo route('members.show', ['id' => 14]); ?>">Members</a>
+    </div>
+    <div class="mb-4 text-[0.5em]/loose">
+        <a href="<?php echo route('tasks.index'); ?>">Tasks</a>
+    </div>
 </x-layout>
